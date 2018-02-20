ffe3cf53d4518780b0e0ed51ca0ae391de3a3a53
Mauro Bolis <bolismauro@gmail.com>
2018-02-20 11:35:46 +0100

    Add initial files

---
+++ documentation.sh
@@ -0,0 +1,6 @@
+jazzy \
+  --author "Bending Spoons Team" \
+  --author_url http://bendingspoons.com \
+  --github_url https://github.com/BendingSpoons/katana-ui-swift \
+  --github-file-prefix https://github.com/BendingSpoons/katana-ui-swift/blob/master/ \
+  --module KatanaUI
